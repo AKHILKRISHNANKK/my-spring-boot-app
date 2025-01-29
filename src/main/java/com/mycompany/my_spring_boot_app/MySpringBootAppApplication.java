f953d80683aba13048f20adfad87c094eff91e3b
shilpacs96 <shilpa.cs96@gmail.com>
2025-01-29 23:54:24 +0530

    changes

--- src/main/java/com/mycompany/my_spring_boot_app/MySpringBootAppApplication.java
+++ src/main/java/com/mycompany/my_spring_boot_app/MySpringBootAppApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MySpringBootAppApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Starting app");
 		SpringApplication.run(MySpringBootAppApplication.class, args);
+
 	}
 
 }
