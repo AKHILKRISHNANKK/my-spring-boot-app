fe6b893eb32f95e5b036073c903ab5102819babe
krishnanakhil327 <krishnanakhil327@gmail.com>
2025-01-29 23:59:14 +0530

    changes

--- src/main/java/com/mycompany/my_spring_boot_app/MySpringBootAppApplication.java
+++ src/main/java/com/mycompany/my_spring_boot_app/MySpringBootAppApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MySpringBootAppApplication {
 
 	public static void main(String[] args) {
-		System.out.println("Starting app");
 		SpringApplication.run(MySpringBootAppApplication.class, args);
 		System.out.println("Stopping the app");
 	}
